7034f1b56221a495fadf0fe7e69aefce2287f1d9
thachanpy <thachanpy@gmail.com>
2020-12-13 17:49:49 +0700

    create upload to s3 script

---
+++ scripts/upload_to_s3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+check_aws=`command -v aws`
+if [[ check_aws == 1 ]]; then
+    apt install awscli -y
+fi
+
+backup_folder=$1
+if [[ $backup_folder == "" ]]; then
+    echo "Need to add folder parameter"
+    exit 1
+fi
+
+for entry in "$backup_folder"/*
+do 
+    if [[ $entry == *.tar ]]; then
+        aws s3 cp $entry s3://thachanpy/backup/
+    fi
+done
